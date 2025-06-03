73950392f33bb7b4de7abdcba8925b8959c2742b
commander15 <128611219+commander15@users.noreply.github.com>
2025-06-03 09:45:53 +0100

    Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # OrderedMultiMap
-Container for ordered enqueue/dequeue on a QMultiMap
+Container for ordered enqueue/dequeue on a QMultiMap.
+
+Here is the result of the program run.
+![Screenshot](./screenshot.png)
+
+* Removed 3 entries in 12432 nano seconds.
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QtCore/qcoreapplication.h>
 #include <QtCore/qtextstream.h>
+#include <QtCore/qelapsedtimer.h>
 
 #include "orderedmultimap.h"
 
@@ -8,20 +9,25 @@ int main(int argc, char *argv[])
     QCoreApplication app(argc, argv);
 
     QTextStream out(stdout);
+    out << "-> User scores <-" << Qt::endl;
 
     OrderedMultiMap<QString, double> scores;
     scores.enqueue("Benjamain", 8.2);
     scores.enqueue("John", 7.8);
-    scores.enqueue("Jean", 7.5);
-    scores.enqueue("Jean", 7.2);
-    scores.enqueue("Jean", 4.5);
+    scores.enqueue("Jean", 7.5); // Jean entry
+    scores.enqueue("Jean", 7.2); // Jean entry
+    scores.enqueue("Jean", 4.5); // Jean entry
     scores.enqueue("Ferry", 5.5);
     scores.enqueue("Ferry", 6.8);
     scores.enqueue("Jules", 5.5);
 
+    QElapsedTimer timer;
+    timer.start();
+
     scores.remove("Jean");
 
-    out << "-> User scores <-" << Qt::endl;
+    out << "*removed 'Jean' entries in " << timer.nsecsElapsed() << " ns" << Qt::endl;
+
     QPair<QString, double> record;
     while (scores.size() > 0) {
         record = scores.dequeuePair();
